268586619a331c679ad330da6a344b86f442fb44
Ivan Kornilov <vano468@gmail.com>
2017-05-21 19:51:52 +0300

    Add logic and more specs for ProfitAnalyzer service

--- app/services/profit_analyzer.rb
+++ app/services/profit_analyzer.rb
@@ -1,13 +1,24 @@
 class ProfitAnalyzer
-  AnalyzedData = Struct.new(:rate_of_return)
+  PERCENT_MONTHS_BASE = 12
+  AnalyzedData = Struct.new(:total_paid, :profit, :profit_rate)
 
   attr_reader :borrower
 
   def initialize(borrower)
     @borrower = borrower
+    @payments = borrower.payments
   end
 
   def analyze
-    AnalyzedData.new(0.30)
+    total_paid  = payments.sum(&:amount)
+    profit      = total_paid - credit_amount
+    profit_rate = profit / credit_amount / credit_term * PERCENT_MONTHS_BASE
+
+    AnalyzedData.new(total_paid, profit, profit_rate)
   end
+
+  private
+
+  attr_reader :payments
+  delegate :credit_amount, :credit_term, to: :borrower
 end
--- spec/services/profit_analyzer_spec.rb
+++ spec/services/profit_analyzer_spec.rb
@@ -13,14 +13,16 @@ describe ProfitAnalyzer do
     end
 
     subject { instance.analyze }
-    let(:rate_of_return) { subject.rate_of_return.round(2).to_f }
+    let(:profit) { subject.profit.round.to_i }
+    let(:profit_rate) { subject.profit_rate.round(2).to_f }
 
     context '6 months/in time' do
       let(:payments) do
         [191_666.67] * 6
       end
 
-      it { expect(rate_of_return).to eq(0.30) }
+      it { expect(profit).to eq(150_000) }
+      it { expect(profit_rate).to eq(0.30) }
     end
 
     context '3 months/in time then early repayment' do
@@ -28,7 +30,8 @@ describe ProfitAnalyzer do
         [191_666.67] * 3 + [525_000.00]
       end
 
-      it { expect(rate_of_return).to eq(0.20) }
+      it { expect(profit).to eq(100_000) }
+      it { expect(profit_rate).to eq(0.2) }
     end
 
     context '2 months/in time then 4 months/delay' do
@@ -36,7 +39,26 @@ describe ProfitAnalyzer do
         [191_666.67] * 2 + [208_333.33] * 4
       end
 
-      it { expect(rate_of_return).to eq(0.43) }
+      it { expect(profit).to eq(216_667) }
+      it { expect(profit_rate).to eq(0.43) }
+    end
+
+    context '6 months/delay' do
+      let(:payments) do
+        [208_333.33] * 6
+      end
+
+      it { expect(profit).to eq(250_000) }
+      it { expect(profit_rate).to eq(0.5) }
+    end
+
+    context '6 months/loss' do
+      let(:payments) do
+        []
+      end
+
+      it { expect(profit).to eq(-1_000_000) }
+      it { expect(profit_rate).to eq(-2.0) }
     end
   end
 end
