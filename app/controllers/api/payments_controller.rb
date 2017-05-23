e1025f83ee095d365da7693c0a4887c5b17339c7
Ivan Kornilov <vano468@gmail.com>
2017-05-23 15:20:15 +0300

    Implement api for payments

--- app/controllers/api/payments_controller.rb
+++ app/controllers/api/payments_controller.rb
@@ -1,3 +1,13 @@
 class Api::PaymentsController < ApiController
+  def create
+    run_operation(Payment::Create, as: :model)
+  end
 
+  def update
+    run_operation(Payment::Update, as: :model)
+  end
+
+  def destroy
+    run_operation(Payment::Destroy)
+  end
 end
